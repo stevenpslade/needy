531a63065ac5913a3c39138a6d6113fca70cff4c
stevenpslade <stevenpslade@gmail.com>
2015-11-12 19:30:06 -0800

    reset password through sorcery

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,23 @@
 class UsersController < ApplicationController
   skip_before_filter :require_login, only: [:index, :new, :create]
-  
+
   def index
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_url, notice: "Welcome aboard, #{@user.email}!"
+    else
+      @user = User.new
+      flash.now[:alert] = 'Login failed'
+      render :new
+    end
   end
 
   def show
@@ -17,6 +27,8 @@ class UsersController < ApplicationController
   end
 
   def destroy
+    logout
+    redirect_to root_path, notice: "You've been logged out"
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
 
-  get 'password_resets/create'
-
-  get 'password_resets/edit'
-
-  get 'password_resets/update'
-
 root :to => 'users#index'
 resources :user_sessions
 resources :users
+resources :password_resets
 
 get 'login' => 'user_sessions#new', :as => :login
 post 'logout' => 'user_sessions#destroy', :as => :logout
