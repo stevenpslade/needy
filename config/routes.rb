4db6a23b434438718ae020d270147b12078e21da
vagrant <vagrant@lighthouse-labs>
2015-11-13 04:22:53 +0000

    Added a primitive fixed, vertical navbar to the layout page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'sessions/new'
 
   resources :users
+  
   root to: 'users#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
