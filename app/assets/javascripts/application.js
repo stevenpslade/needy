110088a4d43e473a5bef64f476093312491c290b
RobertCam <rob.cameron85@gmail.com>
2015-11-15 23:05:23 +0000

    user profile path working based on condition of logged in

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,8 +14,3 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-
- $('.datepicker').pickadate({
-      selectMonths: true, // Creates a dropdown to control month
-      selectYears: 100 // Creates a dropdown of 15 years to control year
-    });
