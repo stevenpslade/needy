9d6f10e897de2afd6f9fafb4ca97f5129e349e09
vagrant <vagrant@lighthouse-labs>
2015-11-15 21:25:52 +0000

    added a 'comments' column to 'Requests' model

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151113081821) do
+ActiveRecord::Schema.define(version: 20151115212309) do
 
   create_table "requests", force: :cascade do |t|
     t.integer  "user_id"
     t.integer  "task_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "comment"
   end
 
   create_table "reviews", force: :cascade do |t|
