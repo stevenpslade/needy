af68312c2faa97ed3051dd151823ffab5eea709a
stevenpslade <stevenpslade@gmail.com>
2015-11-16 17:36:05 -0800

    refactored map html.erb file so it is cleaner and script JS is in JS application file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 
-//= require colorbox-rails
 //= require forms
+//= require maps
 
 $(document).on("page:change", function() {
   
