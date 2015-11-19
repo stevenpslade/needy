97d09a50ff71d966752a6545e13906957a8920ca
stevenpslade <stevenpslade@gmail.com>
2015-11-19 15:21:23 -0800

    chat shows users names and pictures

--- app/assets/javascripts/pusher.js
+++ app/assets/javascripts/pusher.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   current_name = gon.current_user.first_name + " " + gon.current_user.last_name
   other_name = gon.other_user.first_name + " " + gon.other_user.last_name
 
+
   var pusher = new Pusher('dbc5b3802db9df42fe20', {
     authEndpoint: '/pusher/auth',
     auth: {
@@ -16,7 +17,7 @@ $(document).ready(function() {
 
   channel.bind('client-new-message', function(data) {
     $('<div>').addClass('chat-message clearfix')
-      .append('<img src="http://gravatar.com/avatar/2c0ad52fc5943b78d6abe069cc08f320?s=32" alt="" width="32" height="32">')
+      .append('<img src="'+ gon.other_user.profile_image_url +'" alt="" width="32" height="32">')
       .append($('<div>').addClass('chat-message-content clearfix')
         .append($('<span>').addClass('chat-time').html("13:53"))
         .append($('<h5>').text(other_name))
@@ -29,7 +30,7 @@ $(document).ready(function() {
   sendMessage = function(message) {
     channel.trigger('client-new-message', {message: message});
     $('<div>').addClass('chat-message clearfix')
-      .append('<img src="http://gravatar.com/avatar/2c0ad52fc5943b78d6abe069cc08f320?s=32" alt="" width="32" height="32">')
+      .append('<img src="'+ gon.current_user.profile_image_url +'" alt="" width="32" height="32">')
       .append($('<div>').addClass('chat-message-content clearfix')
         .append($('<span>').addClass('chat-time').html("13:53"))
         .append($('<h5>').text(current_name))
