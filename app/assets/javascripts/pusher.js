2877e8e883b408c399309e4e0cb210ca7556efb6
stevenpslade <stevenpslade@gmail.com>
2015-11-18 23:47:27 -0800

    formatting chatbox to show names and pictures, need to reposition

--- app/assets/javascripts/pusher.js
+++ app/assets/javascripts/pusher.js
@@ -13,7 +13,10 @@ $(document).ready(function() {
 
   channel.bind('client-new-message', function(data) {
     $('<div>').addClass('chat-message clearfix')
+      .append('<img src="http://gravatar.com/avatar/2c0ad52fc5943b78d6abe069cc08f320?s=32" alt="" width="32" height="32">')
       .append($('<div>').addClass('chat-message-content clearfix')
+        .append($('<span>').addClass('chat-time').html("13:53"))
+        .append($('<h5>').text('John Smith'))
         .append($('<p>').text(data.message)))
       .appendTo($('.chat-history'))
       $('<hr>').appendTo($('.chat-history'));
@@ -23,7 +26,10 @@ $(document).ready(function() {
   sendMessage = function(message) {
     channel.trigger('client-new-message', {message: message});
     $('<div>').addClass('chat-message clearfix')
+      .append('<img src="http://gravatar.com/avatar/2c0ad52fc5943b78d6abe069cc08f320?s=32" alt="" width="32" height="32">')
       .append($('<div>').addClass('chat-message-content clearfix')
+        .append($('<span>').addClass('chat-time').html("13:53"))
+        .append($('<h5>').text('John Smith'))
         .append($('<p>').text(message)))
       .appendTo($('.chat-history'))
       $('<hr>').appendTo($('.chat-history'))
