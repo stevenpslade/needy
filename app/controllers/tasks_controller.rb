5975f7ef5ef73c9bed23af60b13cf78ab7318fab
stevenpslade <stevenpslade@gmail.com>
2015-11-24 12:22:01 -0800

    format pagination links

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,7 @@ class TasksController < ApplicationController
   skip_before_filter :require_login, only: [:index, :new, :create]
 
   def index
-    @tasks = Task.search(params[:username], params[:query], params[:difficulty], params[:chronology]).page(params[:page]).per(12)
+    @tasks = Task.search(params[:username], params[:query], params[:difficulty], params[:chronology]).page(params[:page]).per(3)
   end
 
   def user_tasks
