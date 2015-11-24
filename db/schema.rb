d20b7b67ad51010a535ff753aa40ce8aac252ac0
vagrant <vagrant@lighthouse-labs>
2015-11-24 17:38:08 +0000

    added the Incomplete Button Form on the 'task/show' page

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-# encoding: UTF-8
+  # encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151123082333) do
+ActiveRecord::Schema.define(version: 20151124165253) do
 
   create_table "chats", force: :cascade do |t|
     t.integer  "task_id"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 20151123082333) do
     t.datetime "created_at",                                                                                                                                  null: false
     t.datetime "updated_at",                                                                                                                                  null: false
     t.string   "title"
+    t.boolean  "incomplete",                default: false
   end
 
   create_table "users", force: :cascade do |t|
